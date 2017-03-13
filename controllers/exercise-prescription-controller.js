0b901269b8876c7621476e59c8d9f41c3b208a11
JiamingLinn <1269761990@qq.com>
2017-03-13 09:21:53 +0800

    处方管理接口

---
+++ controllers/exercise-prescription-controller.js
@@ -0,0 +1,103 @@
+/**
+ * 处方管理
+ * @module exercise-prescription
+ */
+
+const serviceProxy = require('../services/service-proxy');
+const role = "exercise-prescription";
+const prifix = "exercise_prescription";
+
+class ExercisePrescriptionController {
+  // 创建
+  static create(req, res, next) {
+    const data = req.body;
+    serviceProxy.send({ module: role, cmd: `${prifix}_create`, data })
+      .then((result) => {
+        if (result.success) {
+          res.api(result.data);
+        } else {
+          res.error({ code: 40000, msg: result.msg || '创建失败' });
+        }
+      })
+      .catch(next);
+  }
+
+  // 更新 
+  static update(req, res, next) {
+    const data = req.body;
+    const id = req.params.id;
+    Object.assign(data, { id });
+    serviceProxy.send({ module: role, cmd: `${prifix}_update`, data })
+      .then((result) => {
+        if (result.success) {
+          res.api(result.data);
+        } else {
+          res.error({ code: 40000, msg: result.msg || '更新失败' });
+        }
+      })
+      .catch(next);
+  }
+
+  // 查找列表
+  static find(req, res, next) {
+    const data = req.body;
+    serviceProxy.send({ module: role, cmd: `${prifix}_read`, data })
+      .then((result) => {
+        if (result.success) {
+          res.api(result.data);
+        } else {
+          res.error({ code: 40000, msg: result.msg || '查找失败' });
+        }
+      })
+      .catch(next);
+  }
+  
+  // 获取列表总数
+  static count(req, res, next) {
+    const data = req.body;
+    serviceProxy.send({ module: role, cmd: `${prifix}_count`, data })
+      .then((result) => {
+        if (result.success) {
+          res.api(result.data);
+        } else {
+          res.error({ code: 40000, msg: result.msg || '查找失败' });
+        }
+      })
+      .catch(next);
+  }
+
+  // 查找单条记录
+  static findById(req, res, next) {
+    const data = { 
+      id: req.params.id,
+    };
+    Object.assign(data, { id });
+    serviceProxy.send({ module: role, cmd: `${prifix}_read_id`, data })
+      .then((result) => {
+        if (result.success) {
+          res.api(result.data);
+        } else {
+          res.error({ code: 40000, msg: result.msg || '查找失败' });
+        }
+      })
+      .catch(next);
+  }
+
+  // 删除
+  static delete(req, res, next) {
+    const data = { 
+      id: req.params.id,
+    };
+    serviceProxy.send({ module: role, cmd: `${prifix}_delete`, data })
+      .then((result) => {
+        if (result.success) {
+          res.api(result.data);
+        } else {
+          res.error({ code: 40000, msg: result.msg || '删除失败' });
+        }
+      })
+      .catch(next);
+  }
+}
+
+module.exports = ExercisePrescriptionController;
---
+++ routes/auth/exercise-prescription-routes.js
@@ -0,0 +1,30 @@
+/**
+ * 处方管理路由
+ * @module exercise-prescription-routes
+ */
+
+const express = require('express');
+
+const router = express.Router();
+const controller = require('../../controllers/exercise-prescription-controller');
+const utils = require('../../middleware/utils');
+
+// 查询列表
+router.get('/', utils.refsFull, controller.find);
+
+// 查询总数
+router.get('/count', utils.refsFull, controller.count);
+
+// 通过id查询指定记录
+router.get('/:id', controller.findById);
+
+// 创建
+router.post('/', controller.create);
+
+// 更新
+router.put('/:id', controller.update);
+
+// 删除
+router.delete('/:id', controller.delete);
+
+exports = module.exports = router;
