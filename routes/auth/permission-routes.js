07425a8151614993da58df01ba954a95dc88a95d
FengYuHe <heyufeng8080@gmail.com>
2017-04-01 15:57:51 +0800

    add permission depend

---
+++ routes/auth/permission-routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const depend = require('../../configs/permission-depend.json');
+
+const router = express.Router();
+
+// 获取角色权限依赖关系
+router.get('/depend', (req, res) => {
+  res.json(depend);
+});
+
+module.exports = router;
