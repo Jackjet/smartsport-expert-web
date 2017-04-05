4509c19c4b9b52164cd1a06107095dd9f7c66c6d
JiamingLinn <1269761990@qq.com>
2017-04-05 15:33:44 +0800

    修改阿里云生产环境配置

---
+++ utils/aliyun/aliyun-production-config.js
@@ -0,0 +1,13 @@
+const config = {
+  // 基本配置
+  AccessKeyId: ' LTAIyT3gIUT6ZHHG',
+  AccessKeySecret: 'Pg2DgHu4n2xnFEDb9m5GN9lT6CtnaJ',
+  // iot
+  oss: {
+    endpoint: 'https://oss-cn-shenzhen.aliyuncs.com',
+    region: 'oss-cn-shenzhen',
+    smartSportBucket: 'smartsport-root',
+  },
+};
+
+module.exports = config;
